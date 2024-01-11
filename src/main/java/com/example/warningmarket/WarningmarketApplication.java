eb6556a3fb38a54e26aede4b04db51dac6810226
YoonTaeminnnn <xoals4340@hs.ac.kr>
2024-01-11 22:59:09 +0900

    :seedling: "init: 프로젝트 초기 설정"
    dependency & jasypt setting

---
+++ src/main/java/com/example/warningmarket/WarningmarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.warningmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarningmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarningmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/warningmarket/WarningmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warningmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarningmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
