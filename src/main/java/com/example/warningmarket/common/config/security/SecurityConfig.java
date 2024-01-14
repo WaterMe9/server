0978a270f4cbceec573f62662822b8a7e9724052
MoonMinHyuk1 <moon981018@gmail.com>
2024-01-14 16:11:35 +0900

    :seedling: init: security 설정"
    init: security 설정

---
+++ src/main/java/com/example/warningmarket/common/config/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.warningmarket.common.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)       //csrf 보안 설정 사용 X
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(request -> request               // 사용자가 보내는 요청에 인증 절차 수행 필요
+                        .requestMatchers("/", "restdocs/test", "restdocs/get-test").permitAll()    // 해당 URL은 인증 절차 수행 생략 가능
+                        .anyRequest().authenticated())                  // 나머지 요청들은 모두 인증 절차 수행해야함
+
+                .build();
+    }
+}
--- src/main/java/com/example/warningmarket/controller/test/TestController.java
+++ src/main/java/com/example/warningmarket/controller/test/TestController.java
@@ -9,6 +9,11 @@ import java.util.List;
 @RestController
 public class TestController {
 
+    @GetMapping("test")
+    public String test() {
+        return "test";
+    }
+
     @GetMapping("get-test")
     public TestResponse getTest() {
         return TestResponse.builder().name("란란루").build();
