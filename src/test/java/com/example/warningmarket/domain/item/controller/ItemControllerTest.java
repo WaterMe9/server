6e829622ac01237f3eba053889efd68784f25b45
YoonTaeminnnn <xoals4340@hs.ac.kr>
2024-01-30 01:10:26 +0900

    :white_check_mark: Test: 테스트 폴더 구조

---
+++ src/test/java/com/example/warningmarket/domain/item/controller/ItemControllerTest.java
@@ -0,0 +1,6 @@
+package com.example.warningmarket.domain.item.controller;
+
+import com.example.warningmarket.common.AbstractRestDocsTests;
+
+public class ItemControllerTest extends AbstractRestDocsTests {
+}
