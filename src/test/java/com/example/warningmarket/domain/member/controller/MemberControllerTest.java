2e43d461810e22ba4033e86f9d7a8452748ad155
MoonMinHyuk1 <moon981018@gmail.com>
2024-01-30 22:50:22 +0900

    :white_check_mark: test: member test
    test: member test

---
+++ src/test/java/com/example/warningmarket/domain/member/controller/MemberControllerTest.java
@@ -0,0 +1,9 @@
+package com.example.warningmarket.domain.member.controller;
+
+import com.example.warningmarket.common.AbstractRestDocsTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberControllerTest extends AbstractRestDocsTests {
+
+}
---
+++ src/test/java/com/example/warningmarket/domain/member/service/MemberServiceTest.java
@@ -0,0 +1,7 @@
+package com.example.warningmarket.domain.member.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+}
